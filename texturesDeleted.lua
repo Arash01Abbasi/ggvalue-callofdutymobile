14190585ea6daeffdc2d07deeea5391376f9c132
Arash Abbasi <63669353+ArashggAbbasi@users.noreply.github.com>
2022-12-20 23:38:16 +0330

    Create texturesDeleted.lua

---
+++ texturesDeleted.lua
@@ -0,0 +1,5 @@
+--Textures Deleted Game
+gg.setRanges(gg.REGION_C_ALLOC | gg.REGION_ANONYMOUS)
+gg.searchNumber("14D;14D;2:49", gg.TYPE_DOUNLE, false, gg.SIGN_EQUAL, 0, -1)
+gg.searchNumber("2", gg.TYPE_DOUBLE, false, gg.SIGN_EQUAL, 0, -1) gg.getResults(100)
+gg.editAll("5", gg.TYPE_DOUBLE)
